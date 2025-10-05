77e89f108e37cac40fb63c5bea1b6644c4dc8617
Ana Sanchez <anisan1995@gmail.com>
2025-10-04 22:12:06 -0400

    redux functionality

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -6,7 +6,6 @@ import './CartItem.css';
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
-
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
     let total = 0;
