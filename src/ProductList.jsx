08a9404a02fb9fe79434765de253017a0da82c59
Ana Sanchez <anisan1995@gmail.com>
2025-10-04 22:30:22 -0400

    fix

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartSlice'
+import addItem from './CartSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+    const CartItems = useSelector(state => state.cart.items);
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -262,7 +267,6 @@ function ProductList({ onHomeClick }) {
         }));
       };
 
-    dispatch(addItem(product));
     const calculateTotalQuantity = () => {
         return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
          };
@@ -305,7 +309,7 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-title">{plant.name}</div> {/* Display plant name */}
                                 {/* Display other plant details like description and cost */}
                                 <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                                <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                                <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                                 <button
                                     className="product-button"
                                     onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
